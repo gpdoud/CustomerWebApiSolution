d0488939dd9fec98c50ca3f7644d452d021ff9e4
gpdoud <gpdoud@gmail.com>
2017-06-21 10:39:07 -0400

    Add uncommitted changes

---
+++ CustomerWebApiProject/Migrations/201706211333124_initialize.cs
@@ -0,0 +1,30 @@
+namespace CustomerWebApiProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialize : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        DB = c.Int(nullable: false),
+                        Name = c.String(),
+                        Active = c.Boolean(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                        CreditLimit = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ CustomerWebApiProject/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerWebApiProject.Models {
+	public class Customer {
+		public int ID { get; set; }
+		public int DB { get; set; }
+		public string Name { get; set; }
+		public bool Active { get; set; }
+		public DateTime DateCreated { get; set; }
+		public decimal CreditLimit { get; set; }
+	}
+}
